c006b532c89bec5d61c9729b24329ecbfad18a5f
Muneeb Mohammed <mohammedabdulmuneeb98@gmail.com>
2018-06-03 12:23:25 +0300

    Update pruss.h as per the new api spec and add socket functions

--- swig/pruss.h
+++ swig/pruss.h
@@ -1,52 +1,69 @@
 #ifndef PRUSS_H_
 #define PRUSS_H_
 
-#include <iostream>
-#include <cstdlib>
-#include <fstream>
+#include <stdlib.h>
 #include <string>
-#include <sys/mman.h>
-#include <fcntl.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 #include <unistd.h>
 
-#define PRU_OFFLINE 0
-#define PRU_RUNNING 1
-#define PRU_HALTED 2
+//enumeration which describes the states of a PRU Core 
+enum State
+{
+	NONE,
+	STOPPED,
+	RUNNING,
+	HALTED
+};
 
-#define SYSFS_PATH "/sys/class/remoteproc/remoteproc"
-#define FW_PATH "/lib/firmware/am335x-pru"
-#define RPMSG_PATH "/dev/rpmsg_pru"
+class PRUSS
+{
+	private:
+		bool on;
+	
+	public:
+		PRU pru0;
+		PRU pru1;
+		PRUSS();
+		bool isOn();
+		int bootUp();
+		int shutDown();
 
-#define PRU_ICSS 0x4A300000
-#define PRU_ICSS_LEN 512*1024
-#define PRU_DRAM0 0x000
-#define PRU_DRAM1 0x2000
-#define PRU_SRAM 0x10000
+};
 
 class PRU
 {
 	private:
+		State state;
 		int number;
-		int state;
 		std::string channel;
-		std::string sysfs;
-
-	public:
 		PRU(int);
 		PRU(int, std::string);
-		static void modprobe();
-		static void modunprobe();
-		void enable();
-		void disable();
-		void reset();
-		void load(std::string);
-		void set_channel(int);
-		std::string get_state();
-		void send_msg(std::string);
-		std::string get_msg();
-		void wait_for_event();
-		void mem_writeint(int, unsigned int);
-		unsigned int mem_readint(int);
+
+	public:
+		int enable();
+		int disable();
+		int reset();
+		int pause();
+		int resume();
+		std::string showRegs();
+		int load(std::string);
+		int setChannel(int);
+		State getState();
+		int sendMsg(std::string);
+		std::string getMsg();
+		int waitForEvent();
 };
+
+//socket functions
+
+//connect to the socket and return the socket fd 
+int socket_init();
+
+//send the command to the socket server and return the received message
+std::string socket_send(int, std::string);
+
+//close the socket connection.
+void socket_close(int);
 
 #endif
