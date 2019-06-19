f5bea8d8ab4d8ee33afa6e400766068724df6ab1
Pratim Ugale <pratim.ugale@gmail.com>
2019-06-19 12:52:59 +0000

    pru0 was being used instead of pru1

--- examples/rpmsg_echo.cpp
+++ examples/rpmsg_echo.cpp
@@ -6,18 +6,18 @@ using namespace std;
 int main()
 {
 	PRUSS& p = PRUSS::get();
-	PRU p0 = p.pru0;
-	p0.enable();
-	if(!p0.load("./firmware_examples/rpmsg_echo/gen/rpmsg_echo.out"))
+	PRU p1 = p.pru1;
+	p1.enable();
+	/*if(!p0.load("./firmware_examples/rpmsg_echo/gen/rpmsg_echo.out"))
 		cout << "Firmware loaded\n";
 	else
-		return -1;
+		return -1;*/
 	string s;
 	cout << "Enter a message: ";
 	getline(cin, s);
-	p0.sendMsg(s);
-	cout << "Loopback       : "<< p0.getMsg();
-	p0.disable();
+	p1.sendMsg(s);
+	cout << "Loopback       : "<< p1.getMsg();
+	p1.disable();
 	p.shutDown();
 
 }
