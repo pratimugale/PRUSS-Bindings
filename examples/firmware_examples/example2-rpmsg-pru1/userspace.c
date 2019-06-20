335209941fb34739a758ef9c8fcdbca54a9de94d
Pratim Ugale <pratim.ugale@gmail.com>
2019-06-20 20:27:23 +0000

    examples for pruss-api added

---
+++ examples/firmware_examples/example2-rpmsg-pru1/userspace.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2015 Texas Instruments Incorporated - http://www.ti.com/
+ *
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *	* Redistributions of source code must retain the above copyright
+ *	  notice, this list of conditions and the following disclaimer.
+ *
+ *	* Redistributions in binary form must reproduce the above copyright
+ *	  notice, this list of conditions and the following disclaimer in the
+ *	  documentation and/or other materials provided with the
+ *	  distribution.
+ *
+ *	* Neither the name of Texas Instruments Incorporated nor the names of
+ *	  its contributors may be used to endorse or promote products derived
+ *	  from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+//#include <sys/poll.h>
+#include "cpruss.h"
+
+#define MAX_BUFFER_SIZE		512
+char readBuf[MAX_BUFFER_SIZE];
+
+#define NUM_MESSAGES		100
+//#define DEVICE_NAME		"/dev/rpmsg_pru31"
+
+int main(void)
+{
+	int i;
+	int result = 0;
+	/*
+	 * If the RPMsg channel doesn't exist yet the character device
+	 * won't either.
+	 * Make sure the PRU firmware is loaded and that the rpmsg_pru
+	 * module is inserted.
+	 */
+
+        //restart(1);
+	for (i = 0; i < NUM_MESSAGES; i++) {
+		/* Send 'pratim ugale' to the PRU through the RPMsg channel */
+		result = send_msg("pratim ugale", 1);
+                // result(message, prun)
+		if (result > 0)
+			printf("Message %d: Sent to PRU\n", i);
+
+		char *message = get_msg(1);
+                if (message != NULL)
+		    printf("Message %d received from PRU:%s\n\n", i, message);
+	}
+	return 0;
+}
+
