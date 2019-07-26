3efd2b1b2e870256ef06e041d20ac3921f43404b
Pratim Ugale <pratim.ugale@gmail.com>
2019-07-26 12:21:46 +0000

    4-channel waveform gen

--- examples/firmware_examples/example9-multichannel-waveform-gen/PRU0/pulse-supply1.asm
+++ examples/firmware_examples/example9-multichannel-waveform-gen/PRU0/pulse-supply1.asm
@@ -11,29 +11,24 @@ PRU_SRAM .set 0x00010000            ; Set the location of PRU Shared Memory
 start:                              ; One time setup.
         LDI32   R10, PRU_SRAM       ; R10 -> Base address of PRU SRAM
         SUB     R5, R5, R5          ; Clear the contents of R5
-        ;LBBO    &R5, R10, 0, 1      ; R5 -> Total number of samples including all the waveforms.
-        LDI     R5, 400
+        LBBO    &R5, R10, 0, 1      ; R5 -> Total number of samples including all the waveforms.
         LDI     R6, 100             ; Total number of cycles for each individual wave sample.
-        LDI     R11, 0              ; Total Sample Counter for all Waveforms.
+        LDI     R11, 0              ; Sample Counter.
         QBA     sample_start
         SUB     R16, R16, R16
 
 sample_start:                       ; 
         
-        QBLT    same_sample, R5, R11
+        QBLT    same_sample, R5.b0, R11.b0
         LDI     R11, 0
 
-same_sample:                          ; 
+same_sample:                        ; 
         ADD     R11, R11, 0x00000001  ; Increment offset counter by 1 
-        LBBO    &R7.b0, R10, R11, 1   ; R7 -> DC of Wave 1
+        LBBO    &R7.b0, R10, R11.b0, 1; R7 -> DC of Wave 1
         ADD     R11, R11, 0x00000001  ; Increment offset counter by 1 
-        LBBO    &R8.b0, R10, R11, 1   ; R8 -> DC of Wave 2
-        ADD     R11, R11, 0x00000001  ; Increment offset counter by 1 
-        LBBO    &R7.b1, R10, R11, 1   ; R8 -> DC of Wave 3
-        ADD     R11, R11, 0x00000001  ; Increment offset counter by 1 
-        LBBO    &R8.b1, R10, R11, 1   ; R8 -> DC of Wave 3
-        LDI32   R12, 2500             ; 
-        LDI     R9,  0                ; DC ON counter.
+        LBBO    &R8.b0, R10, R11.b0, 1; R8 -> DC of Wave 2
+        LDI32   R12, 10000
+        LDI     R9, 0                ; DC ON counter.
         LDI     R15, 0                ; DC OFF counter.
         LDI32   R13, 0                ; Pulse counter.
         QBLT    greaterthan_zero1, R7.b0, 1
@@ -48,24 +43,8 @@ lesserthan_hun1:
         LDI     R8.b0, 1
         
 greaterthan_zero2:
-        QBGT    lesserthan_hun2, R8.b0, 99
+        QBGT    count_check, R8.b0, 99
         LDI     R8.b0, 99
-
-lesserthan_hun2:
-        QBLT    greaterthan_zero3, R7.b1, 1
-        LDI     R7.b1, 1
-
-greaterthan_zero3:
-        QBGT    lesserthan_hun3, R7.b1, 99
-        LDI     R7.b1, 99
-
-lesserthan_hun3:
-        QBLT    greaterthan_zero4, R8.b1, 1
-        LDI     R8.b1, 1
-
-greaterthan_zero4:
-        QBGT    count_check, R8.b1, 99
-        LDI     R8.b1, 99
 
 count_check:
         ADD     R13, R13, 1     
@@ -73,7 +52,7 @@ count_check:
 
 start_pwm1: 
         SET     R30, R30.t0
-        SUB     R14.b0, R6.b0, R7.b0
+        SUB     R14, R6, R7 
         
 pwm1_high:
         ADD     R9, R9, 0x00000001
@@ -88,7 +67,7 @@ pwm1_low:
 
 start_pwm2: 
         SET     R30, R30.t2
-        SUB     R14.b0, R6.b0, R8.b0
+        SUB     R14, R6, R8
  
 pwm2_high:
         ADD     R9, R9, 0x00000001
@@ -100,37 +79,6 @@ pwm2_low:
         QBNE    pwm2_low, R15.b0, R14.b0
         SUB     R15, R15, R15
         SUB     R9, R9, R9
-
-start_pwm3: 
-        SET     R30, R30.t1
-        SUB     R14.b0, R6.b0, R7.b1
-        
-pwm3_high:
-        ADD     R9, R9, 0x00000001
-        QBNE    pwm3_high, R9.b0, R7.b1
-        CLR     R30, R30.t1
-
-pwm3_low:
-        ADD     R15, R15, 0x00000001
-        QBNE    pwm3_low, R15.b0, R14.b0
-        SUB     R15, R15, R15
-        SUB     R9, R9, R9
-
-start_pwm4: 
-        SET     R30, R30.t3
-        SUB     R14.b0, R6.b0, R8.b1
- 
-pwm4_high:
-        ADD     R9, R9, 0x00000001
-        QBNE    pwm4_high, R9.b0, R8.b1
-        CLR     R30, R30.t3
-
-pwm4_low:
-        ADD     R15, R15, 0x00000001
-        QBNE    pwm4_low, R15.b0, R14.b0
-        SUB     R15, R15, R15
-        SUB     R9, R9, R9
-
         QBA     count_check
 
 done_sample:
--- examples/firmware_examples/example9-multichannel-waveform-gen/userspace.cpp
+++ examples/firmware_examples/example9-multichannel-waveform-gen/userspace.cpp
@@ -12,26 +12,38 @@ int main()
     PRU p1 = p.pru1;
     p1.enable();            // Starts PRU1 (rpmsg_pru31 character device file is created here)
 
-    uint8_t noOfChannels = 2;
+    uint8_t noOfChannels = 4;
 
     uint8_t i;
     uint8_t waveform1[100]; 
     uint8_t waveform2[100]; 
+    uint8_t waveform3[100]; 
+    uint8_t waveform4[100]; 
     float gain = 50.0f;
     float phase1 = 0.0f;
-    float phase2 = 1.0f * (float)PI;
+    float phase2 = 1.0f * (float)PI/4;
+    float phase3 = 2.0f * (float)PI/4;
+    float phase4 = 3.0f * (float)PI/4;
     float bias = 50.0f;
     float freq = 2.0f * (float)PI / 100.0f;
     
     for (i = 0; i < 100; i++){
         waveform1[i] = (uint8_t)((bias + (gain * sin((i*freq) + phase1))));
     }
+
     for (i = 0; i < 100; i++){
-        waveform2[i] = i*i/100;
-       // waveform2[i] = (uint8_t)((bias + (gain * sin((i*freq) + phase2))));
+        //waveform2[i] = i*i/100;
+        waveform2[i] = (uint8_t)((bias + (gain * sin((i*freq) + phase2))));
     }
 
+    for (i = 0; i < 100; i++){
+        waveform3[i] = (uint8_t)((bias + (gain * sin((i*freq) + phase3))));
+    }
     
+    for (i = 0; i < 100; i++){
+        waveform4[i] = (uint8_t)((bias + (gain * sin((i*freq) + phase4))));
+    }
+
     uint8_t numbersamples = 100*noOfChannels;
 
     p1.sendMsg_raw(to_string(numbersamples));
@@ -39,6 +51,8 @@ int main()
     for(i = 0; i < 100; i++){
             p1.sendMsg_raw(to_string(waveform1[i]));
             p1.sendMsg_raw(to_string(waveform2[i]));
+            p1.sendMsg_raw(to_string(waveform3[i]));
+            p1.sendMsg_raw(to_string(waveform4[i]));
     }
 
     p1.disable();
